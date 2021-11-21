9fa89b8c562ee9f00fe0c4e61fab5feeff1966f6
Dario Velez Roman <dvelezroman@gmail.com>
2021-11-21 01:27:26 -0300

    add eslint

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = new express.Router();
 
 /* GET test PING route. */
 router.get('/ping', async (req, res) => {
@@ -8,18 +8,18 @@ router.get('/ping', async (req, res) => {
     const pongResponse = {
       status: true,
       body: {
-        message: 'PONG'
-      }
-    }
-    res.status(200).send(pongResponse)
+        message: 'PONG',
+      },
+    };
+    res.status(200).send(pongResponse);
   } catch (Error) {
-    res.status(500).send({ Error: 'Something went wrong.' })
+    res.status(500).send({ Error: 'Something went wrong.' });
   }
-})
+});
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' })
-})
+  res.render('index', { title: 'Express' });
+});
 
-module.exports = router
+module.exports = router;
