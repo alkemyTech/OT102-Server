6f7160a046f014c3a7a5fae7e467d62df8a09f0f
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-11-23 13:00:27 -0300

    change model Category 'description' field dataType to TEXT

--- models/category.js
+++ models/category.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
         unique: true,
         allowNull: false,
       },
-      description: DataTypes.STRING,
+      description: DataTypes.TEXT,
       deletedAt: DataTypes.DATE,
     },
     {
