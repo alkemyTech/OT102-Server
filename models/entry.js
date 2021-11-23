ff3945c3b90c4e9d6141a878692679942974af57
joselo85 <joseignaciolorenzo85@gmail.com>
2021-11-23 19:40:44 -0300

    Fixed typos.

--- models/entry.js
+++ models/entry.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.belongsTo(models.Categoy, { as: 'category' });
+      Entry.belongsTo(models.Category, { as: 'category' });
     }
   }
   Entry.init(
