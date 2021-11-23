d7c3024838e24102b4468926c382e3300a4d04ad
gabodmc <gabrielmc@live.com.ar>
2021-11-23 18:47:21 -0300

    Asociation with model User added

--- models/testimonial.js
+++ models/testimonial.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+    Testimonial.belongsTo(models.User);
     }
   };
   Testimonial.init({
