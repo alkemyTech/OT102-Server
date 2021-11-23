fa025123565d66817562e1569c1bede40bfb1fc7
joselo85 <joseignaciolorenzo85@gmail.com>
2021-11-23 11:14:54 -0300

    Created Entry Model and migration

---
+++ migrations/20211123135111-create-entry.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Entries', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Entries');
+  },
+};
