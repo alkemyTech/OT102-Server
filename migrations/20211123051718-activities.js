32dc8c2119aface03d4d1f1001fd18217ec7acae
Gabriel <gabriel99luques@gmail.com>
2021-11-23 03:49:06 -0300

    migration-model-seeders of the activities table

---
+++ migrations/20211123051718-activities.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Activities', {
+      id:{
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name:{
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image:{
+        type: Sequelize.STRING
+      },
+      content:{
+        type: Sequelize.STRING
+      },
+      deletedAt:{
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    
+      await queryInterface.dropTable('Activities');
+     
+  }
+};
