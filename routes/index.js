6c6f1f0b633e688def548306a81478f9c0459116
Cheaguz <aguzf83@gmail.com>
2021-11-24 21:50:53 -0300

    Organizations added

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = new express.Router()
 const usersRouter = require('./user')
 const pingRouter = require('./ping')
 const categoriesRouter = require('./categories')
+const organizationsRouter = require('./organizations')
 
 router.use('/users', usersRouter)
 router.use('/ping', pingRouter)
 router.use('/categories', categoriesRouter)
+router.use('/organizations', organizationsRouter)
 
 module.exports = router
