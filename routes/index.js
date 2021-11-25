a2bd3ec169d339313bdc0469e3767e77b8248468
alexiscjscab <alexiscjscab@gmail.com>
2021-11-25 16:04:21 -0300

    Routes contacts

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = new express.Router()
 const usersRouter = require('./user')
 const pingRouter = require('./ping')
 const categoriesRouter = require('./categories')
+const contactsRouter = require('./contacts')
 
 router.use('/users', usersRouter)
 router.use('/ping', pingRouter)
 router.use('/categories', categoriesRouter)
+router.use('/contacts', contactsRouter)
 
 module.exports = router
