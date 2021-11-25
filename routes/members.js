9421093f7b563eef23f1d375105c8e6680f7e05a
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-11-25 20:51:28 -0300

    members route created

--- routes/members.js
+++ routes/members.js
@@ -1,13 +1,31 @@
 const express = require('express')
 
+const { endpointResponse } = require('../helpers/success')
+const { ErrorObject } = require('../helpers/error')
+const { Member } = require('../models')
+const { catchAsync } = require('../helpers')
+
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-  try {
-    res.status(200).send()
-  } catch (e) {
-    throw Error('Error while retrieving members')
-  }
-})
+router.get(
+  '/',
+  catchAsync(async (req, res, next) => {
+    try {
+      const allMembers = await Member.findAll()
+      endpointResponse({
+        res,
+        message: 'Members were retrieved successfully.',
+        body: allMembers,
+      })
+    } catch (error) {
+      next(
+        new ErrorObject(
+          `[Error retrieving members] - [members - get]: ${error.message}`,
+          500,
+        ),
+      )
+    }
+  }),
+)
 
 module.exports = router
