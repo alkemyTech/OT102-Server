4cbd5ff85f5c8b32d9f6d89a86083c7c63f2b69a
agustin <aguzf83@gmail.com>
2021-11-25 14:32:13 -0300

    organization controller update

--- controllers/organizationsController.js
+++ controllers/organizationsController.js
@@ -1,17 +1,22 @@
 const { getById } = require('../services/organizationServices')
+const { ErrorObject } = require('../helpers/error')
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/index')
 
 module.exports = {
-  getById: async (req, res, next) => {
+  getById: catchAsync(async (req, res, next) => {
     try {
-      const organizations = await getById(req.params.id)
-      const response = {
-        status: true,
-        msg: 'Organizations retrieved successfully',
-        data: organizations,
-      }
-      res.status(200).json(response)
+      const organization = await getById(req.params.id)
+      /* if (organization) {
+        return next(new ErrorObject('No organization were found with that ID', 404))
+      } */
+      endpointResponse({
+        res,
+        message: 'Organization retrieved successfully.',
+        body: organization,
+      })
     } catch (error) {
-      next(error)
+      next(new ErrorObject(`[Error retrieving organization] - [organization - get]: ${error.message}`, 404))
     }
-  },
+  }),
 }
