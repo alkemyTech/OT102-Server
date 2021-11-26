2c8071fbda1e6fbf115972143fbc937d80a6802d
gabyMangoni <79479608+gabyMangoni@users.noreply.github.com>
2021-11-26 18:57:10 -0300

    [ENDPOINT] GET / news / OT102-45 (#23)
    [ENDPOINT] GET / news / OT102-45 (#23)

--- routes/entries.js
+++ routes/entries.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { getEntry } = require('../controllers/entry')
+const { get, getEntry } = require('../controllers/entry')
 
 const router = new express.Router()
 
+router.get('/', get)
 router.get('/:id', getEntry)
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,17 @@ const router = new express.Router()
 const usersRouter = require('./user')
 const pingRouter = require('./ping')
 const categoriesRouter = require('./categories')
-
-const organizationsRouter = require('./organizations')
 const membersRouter = require('./members')
 const entriesRouter = require('./entries')
+
+const organizationsRouter = require('./organizations')
 const contactsRouter = require('./contacts')
 
 router.use('/users', usersRouter)
 router.use('/ping', pingRouter)
 router.use('/categories', categoriesRouter)
+router.use('/members', membersRouter)
+router.use('/news', entriesRouter)
 
 router.use('/organizations', organizationsRouter)
 router.use('/members', membersRouter)
