129d2e20f26417e1ed740b79ed07ade7c7bd3529
Cheaguz <aguzf83@gmail.com>
2021-11-25 22:19:51 -0300

    fix if

--- controllers/organizationsController.js
+++ controllers/organizationsController.js
@@ -7,18 +7,15 @@ module.exports = {
   getById: catchAsync(async (req, res, next) => {
     try {
       const organization = await getById(req.params.id)
-       if (!organization) {
-        return next(new ErrorObject('No organization were found with that ID', 404))
-      } 
       endpointResponse({
         res,
         message: 'Organization retrieved successfully.',
         body: {
           name: organization.name,
           image: organization.image,
-          phone: organization.phone, 
+          phone: organization.phone,
           address: organization.adress,
-          welcomeText: organization.welcomeText
+          welcomeText: organization.welcomeText,
         },
       })
     } catch (error) {
