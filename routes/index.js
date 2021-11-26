1212eda71913e57b8bb39944e1a2b9c179a1080c
Jonatan Garbuyo <58077441+JonatanGarbuyo@users.noreply.github.com>
2021-11-26 10:16:54 -0300

    [ENDPOINT]: GET /members - OT102-89 (#21)
    * add route and test for endpoint members
    
    * members route created
    
    * extract retrieve of members to a function
    
    * extract retrieve of members into a service
    
    * extract get members into a controller
    
    * add comment

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const router = new express.Router()
 const usersRouter = require('./user')
 const pingRouter = require('./ping')
 const categoriesRouter = require('./categories')
+const membersRouter = require('./members')
 const entriesRouter = require('./entries')
 
 router.use('/users', usersRouter)
 router.use('/ping', pingRouter)
 router.use('/categories', categoriesRouter)
+router.use('/members', membersRouter)
 router.use('/news', entriesRouter)
 
 module.exports = router
