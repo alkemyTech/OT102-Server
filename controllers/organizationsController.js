1c0db8a7edc15fcbdeeb56c6b8a6968f8c7a8fa0
Cheaguz <aguzf83@gmail.com>
2021-11-25 21:48:32 -0300

    services and controllers finished

--- controllers/organizationsController.js
+++ controllers/organizationsController.js
@@ -7,13 +7,19 @@ module.exports = {
   getById: catchAsync(async (req, res, next) => {
     try {
       const organization = await getById(req.params.id)
-      /* if (organization) {
+       if (!organization) {
         return next(new ErrorObject('No organization were found with that ID', 404))
-      } */
+      } 
       endpointResponse({
         res,
         message: 'Organization retrieved successfully.',
-        body: organization,
+        body: {
+          name: organization.name,
+          image: organization.image,
+          phone: organization.phone, 
+          address: organization.adress,
+          welcomeText: organization.welcomeText
+        },
       })
     } catch (error) {
       next(new ErrorObject(`[Error retrieving organization] - [organization - get]: ${error.message}`, 404))
