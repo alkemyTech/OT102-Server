bda12b8296007eb48d21d96f728b62d7fb647cc2
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-11-25 21:14:58 -0300

    extract get members into a controller

--- routes/members.js
+++ routes/members.js
@@ -1,31 +1,8 @@
 const express = require('express')
-
-const { endpointResponse } = require('../helpers/success')
-const { ErrorObject } = require('../helpers/error')
-const { catchAsync } = require('../helpers')
-const { getMembers } = require('../services/members')
+const { get } = require('../controllers/members')
 
 const router = express.Router()
 
-router.get(
-  '/',
-  catchAsync(async (req, res, next) => {
-    try {
-      const allMembers = await getMembers()
-      endpointResponse({
-        res,
-        message: 'Members were retrieved successfully.',
-        body: allMembers,
-      })
-    } catch (error) {
-      next(
-        new ErrorObject(
-          `[Error retrieving members] - [members - get]: ${error.message}`,
-          500,
-        ),
-      )
-    }
-  }),
-)
+router.get('/', get)
 
 module.exports = router
