462d55d5c8adc3082a80df878ee183f0a44d8964
AgustinFernandez <67702629+cheaguz@users.noreply.github.com>
2021-11-25 22:21:50 -0300

    Update organizationsController.js
    fix if

--- controllers/organizationsController.js
+++ controllers/organizationsController.js
@@ -7,9 +7,6 @@ module.exports = {
   getById: catchAsync(async (req, res, next) => {
     try {
       const organization = await getById(req.params.id)
-       if (!organization) {
-        return next(new ErrorObject('No organization were found with that ID', 404))
-      } 
       endpointResponse({
         res,
         message: 'Organization retrieved successfully.',
