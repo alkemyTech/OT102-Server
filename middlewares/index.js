9ec0f157c98a9e76fd5eb703f642cef95f6ecd50
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-11-28 01:44:32 -0300

    add validateRequest middleware function

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator')
+
 const { ErrorObject } = require('../helpers/error')
 
 const validateUser = async (req, res, next) => {
@@ -12,6 +14,27 @@ const validateUser = async (req, res, next) => {
   }
 }
 
+// Validate request
+const validate = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    res
+      .status(400)
+      .json(
+        new ErrorObject(
+          `[Error validating ${req.baseUrl.slice(1)}] - [${req.baseUrl} - ${
+            req.method
+          }]`,
+          400,
+          errors.array(),
+        ),
+      )
+  }
+  next()
+}
+const validateRequest = (schema) => [schema, validate]
+
 module.exports = {
+  validateRequest,
   validateUser,
 }
