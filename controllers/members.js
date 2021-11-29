29090a50db406491d9ce2a60beb29390d4491e37
gabodmc <gabrielmc@live.com.ar>
2021-11-29 16:28:17 -0300

    Delete member route/model/service

--- controllers/members.js
+++ controllers/members.js
@@ -1,7 +1,7 @@
 const { catchAsync } = require('../helpers')
 const { endpointResponse } = require('../helpers/success')
 const { ErrorObject } = require('../helpers/error')
-const { getMembers } = require('../services/members')
+const { getMembers, deleteMember } = require('../services/members')
 
 module.exports = {
   get: catchAsync(async (req, res, next) => {
@@ -16,6 +16,23 @@ module.exports = {
       next(
         new ErrorObject(
           `[Error retrieving members] - [members - get]: ${error.message}`,
+          500,
+        ),
+      )
+    }
+  }),
+  destroy: catchAsync(async (req, res, next) => {
+    try {
+      const deletedMember = await deleteMember(req.params.id)
+      endpointResponse({
+        res,
+        message: 'Member succesfully deleted',
+        body: deletedMember,
+      })
+    } catch (error) {
+      next(
+        new ErrorObject(
+          `[Error deleting Member] - [memeber - delete]: ${error.message}`,
           500,
         ),
       )
--- services/members.js
+++ services/members.js
@@ -7,3 +7,19 @@ exports.getMembers = async () => {
     throw Error(error.message)
   }
 }
+
+exports.deleteMember = async (id) => {
+  try {
+    const findMember = await Member.findOne({
+      where: { id },
+    })
+    if (!findMember) {
+      throw Error('Member already deleted/Invalid ID')
+    } else {
+      const deletedMember = await Member.destroy({ where: { id } })
+      return deletedMember
+    }
+  } catch (error) {
+    throw Error(error.message)
+  }
+}
