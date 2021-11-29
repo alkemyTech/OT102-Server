e159f7659e76390a4459fdb69cb33656fe14dcbc
maxicuyo94 <maxibc94@gmail.com>
2021-11-29 19:27:29 -0300

    use middleware validate and delete duplicate route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const { get, addContact } = require('../controllers/contacts')
+const { validateRequest } = require('../middlewares')
+const { contactSchema } = require('../schemas/contacs')
 
 const router = new express.Router()
 
 router.get('/', get)
-router.post('/', addContact)
+router.post('/', validateRequest(contactSchema), addContact)
 
 module.exports = router
