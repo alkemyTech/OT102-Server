ae7cef72efb44f863ce16204f1ab0a3801d7eada
maxicuyo94 <maxibc94@gmail.com>
2021-11-29 09:42:51 -0300

    use http error handler

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,21 +1,24 @@
 // midleware to check if the user.roleId user is admin
 const jwt = require('jsonwebtoken')
+const createHttpError = require('http-errors')
 const config = require('../config/config').development
-const ErrorObject = require('../helpers/error')
 
 const isAdmin = (req, res, next) => {
   const token = req.headers['x-access-token']
   if (!token) {
-    next(new ErrorObject('No token provided.', 401))
+    const httpError = createHttpError(401, 'No token provided')
+    next(httpError)
   }
   jwt.verify(token, config.secret, (err, decoded) => {
     if (err) {
-      next(new ErrorObject('Failed to authenticate token.', 401))
+      const httpError = createHttpError(401, 'Failed to authenticate token')
+      next(httpError)
     }
     if (decoded.role === 'Admin') {
       next()
     } else {
-      next(new ErrorObject('You are not an admin', 403))
+      const httpError = createHttpError(401, 'Forbidden access')
+      next(httpError)
     }
     return null
   })
