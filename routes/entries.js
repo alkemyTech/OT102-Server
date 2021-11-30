ea25a5cd280d5f63e53f98c7a52b04195f42eb13
gabyMangoni <mangonigabriela@gmail.com>
2021-11-29 23:00:06 -0300

    add isAdmin

--- routes/entries.js
+++ routes/entries.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { get, getEntry, destroy } = require('../controllers/entry')
+const isAdmin = require('../middlewares/isAdmin')
 
 const router = new express.Router()
 
 router.get('/', get)
 router.get('/:id', getEntry)
-router.delete('/:id', destroy)
+router.delete('/:id', isAdmin, destroy)
 
 module.exports = router
