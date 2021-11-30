ac1cd04f17baeb5008fe7dd2ae5aa18e34715204
gabodmc <gabrielmc@live.com.ar>
2021-11-29 21:21:21 -0300

    isAdmin middleware added

--- routes/members.js
+++ routes/members.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const isAdmin = require('../middlewares/isAdmin')
 const { get, destroy } = require('../controllers/members')
 
 const router = express.Router()
 
 /* GET all active members */
 router.get('/', get)
-router.delete('/:id', destroy)
+router.delete('/:id', isAdmin, destroy)
 
 module.exports = router
