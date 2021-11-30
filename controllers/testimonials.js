738b7d6a22a05d9ed7cbd1941abfb5e3f39007f5
Jose Ignacio Lorenzo <74208929+joselo85@users.noreply.github.com>
2021-11-29 21:44:48 -0300

    [ENDPOINT] PUT /testimonials/:id - OT102-85 (#29)
    * Initial Commit
    
    * Fixed minor bugs
    
    * Changed error message.
    
    * Changed return value to suit ticket requirement
    
    * Fixed Error handling. Refactored code.
    
    * Refactored code.
    
    * Fixed typos
    
    * Updated with isAdmin middleware

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -1,8 +1,7 @@
 const createHttpError = require('http-errors')
-
-const { catchAsync } = require('../helpers')
+const { getAllTestimonials, updateById } = require('../services/testimonials')
 const { endpointResponse } = require('../helpers/success')
-const { getAllTestimonials } = require('../services/testimonials')
+const { catchAsync } = require('../helpers/index')
 
 module.exports = {
   get: catchAsync(async (req, res, next) => {
@@ -15,6 +14,24 @@ module.exports = {
         status: true,
         message: 'Testimonials retrieved successfully',
         body: testimonials,
+      })
+    } catch (error) {
+      const httpError = createHttpError(error.statusCode, error.message)
+      next(httpError)
+    }
+  }),
+  updateTestimonial: catchAsync(async (req, res, next) => {
+    try {
+      const updatedTestimonial = await updateById(
+        req.params.id,
+        req.body.name,
+        req.body.content,
+      )
+
+      endpointResponse({
+        res,
+        message: 'Testimonial succesfully updated',
+        body: updatedTestimonial,
       })
     } catch (error) {
       const httpError = createHttpError(error.statusCode, error.message)
