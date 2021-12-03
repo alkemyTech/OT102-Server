5612c7505f7f7bb33fa2cecf5407877140291e27
maxicuyo94 <maxibc94@gmail.com>
2021-12-03 10:49:12 -0300

    add template

--- helpers/index.js
+++ helpers/index.js
@@ -1,31 +1,7 @@
 const catchAsync = (fn) => (req, res, next) => {
   fn(req, res, next).catch(next)
 }
-// sendgrid mail sender
-const sgMail = require('@sendgrid/mail')
-const createHttpError = require('http-errors')
-const config = require('../config/config').development
-
-const sendMail = async (to = '', subject = '', message = '', html = '') => {
-  sgMail.setApiKey(config.sendGridKey)
-  const msg = {
-    to,
-    from: config.sendGridMail,
-    subject,
-    text: message,
-    html,
-  }
-
-  try {
-    await sgMail.send(msg)
-    return true
-  } catch (error) {
-    const httpError = createHttpError(500, `Error sending mail: ${error}`)
-    throw httpError
-  }
-}
 
 module.exports = {
   catchAsync,
-  sendMail,
 }
