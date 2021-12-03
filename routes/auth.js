528317903a31564c65916197c2c1ca2027841b5f
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-12-03 09:12:14 -0300

    add /auth/me  route, controller and service

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { createUser } = require('../controllers/users')
+const { createUser, getMyUser } = require('../controllers/users')
 const { validateRequest } = require('../middlewares')
+const userExtractor = require('../middlewares/userExtractor')
 const { userRegisterSchema } = require('../schemas/users')
 
 const router = new express.Router()
 
 router.post('/register', validateRequest(userRegisterSchema), createUser)
+router.get('/me', userExtractor, getMyUser)
 
 module.exports = router
