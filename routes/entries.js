116922e8746af1e45cc0e55eb1c4cd741558ae6d
Jonatan Garbuyo <58077441+JonatanGarbuyo@users.noreply.github.com>
2021-12-09 12:08:37 -0300

    [ENDPOINT] - POST /news - OT102-55 (#58)
    * post new entry added
    
    * add POST /news endpoint
    
    Co-authored-by: Cheaguz <aguzf83@gmail.com>

--- routes/entries.js
+++ routes/entries.js
@@ -1,19 +1,23 @@
 const express = require('express')
+
 const {
   get,
   getEntry,
   destroy,
   updatedEntry,
+  post,
 } = require('../controllers/entry')
 const { validateRequest } = require('../middlewares')
 const isAdmin = require('../middlewares/isAdmin')
-const { updateEntry } = require('../schemas/entries')
+
+const { entrySchema } = require('../schemas/entries')
 
 const router = new express.Router()
 
 router.get('/', get)
 router.get('/:id', getEntry)
-router.put('/:id', [isAdmin, validateRequest(updateEntry)], updatedEntry)
+router.put('/:id', [isAdmin, validateRequest(entrySchema)], updatedEntry)
+router.post('/', [isAdmin, validateRequest(entrySchema)], post)
 router.delete('/:id', isAdmin, destroy)
 
 module.exports = router
