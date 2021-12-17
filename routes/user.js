7dc238548aed17ac25f0410846604daac0f7d2e0
maxicuyo94 <maxibc94@gmail.com>
2021-12-17 18:31:55 -0300

    change route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const { get, destroy } = require('../controllers/users')
+const { get, destroy, updateUser } = require('../controllers/users')
 
 const router = new express.Router()
 
 /* GET users listing. */
 router.get('/', get)
 router.delete('/:id', destroy)
-router.put('/:id', destroy)
+router.put('/:id', updateUser)
 module.exports = router
