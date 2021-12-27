92fb2f8027ecf1606969f98f8954a2dc1498ca6d
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2021-12-27 10:35:43 -0300

    refactor code

--- test/members.test.js
+++ test/members.test.js
@@ -9,14 +9,15 @@ const {
 app = require('../app')
 
 describe('/members', () => {
-  /* Define the happy path, and then in each test, 
-  we change one parameter that clear aligns with the name of the test. */
+  /* Define the parameters, and then in each test, 
+  change one parameter that clear aligns with the name of the test. */
   const URL = '/members'
-  const member = { name: 'Johnn Doe', image: 'some.image.url' }
   let token
+  let member
 
   beforeEach(() => {
     // example use for beforeEach
+    member = { name: 'Johnn Doe', image: 'some.image.url' }
     token = generateToken({ name: 'test', userRole: 'Admin' })
   })
 
@@ -27,41 +28,38 @@ describe('/members', () => {
 
   //  Using the 3A pattern. Arrange, Act, Assert.
   describe('POST /', () => {
-    it('should reject the POST request without token', async () => {
+    it('should reject the POST request from non admin user', async () => {
       /* Arrange: This is where you initialize objects and set the value of the data
       to be passed to the function under test. This is also the best place
       to set up mocks if we intend to use them. */
-      const member = { name: 'Johnn Doe', image: 'some.image.url' }
+      token = generateToken({ name: 'test' })
 
       /* Act: The act section invokes the function under test. */
-      const response = await request(app).post(URL).send(member)
+      const response = await request(app)
+        .post(URL)
+        .set({ 'x-access-token': token })
+        .send(member)
 
       /* Assert: This is where you verify that the action of the function under
       test behaves as expected. */
+      expect(response.status).to.be.equal(401) // fix middleware. code must be 403
+      expect(response.body)
+        .to.have.property('message')
+        .match(/forbidden/i)
+    })
+
+    it('should reject the POST request without token', async () => {
+      /* Act */
+      const response = await request(app).post(URL).send(member)
+
       expect(response.status).to.be.equal(401)
       expect(response.body)
         .to.have.property('message')
         .match(/no token/i)
     })
 
-    it('should reject the POST request from non admin user', async () => {
-      const member = { name: 'Johnn Doe', image: 'some.image.url' }
-      token = generateToken({ name: 'test' })
-
-      const response = await request(app)
-        .post(URL)
-        .set({ 'x-access-token': token })
-        .send(member)
-
-      // fix middleware. code must be 403
-      expect(response.status).to.be.equal(401)
-      expect(response.body)
-        .to.have.property('message')
-        .match(/forbidden/i)
-    })
-
     it('should reject the POST request with missing name property', async () => {
-      const member = { image: 'some.image.url' }
+      delete member.name
 
       const response = await request(app)
         .post(URL)
@@ -76,7 +74,7 @@ describe('/members', () => {
     })
 
     it('should reject the POST request with missing image property', async () => {
-      const member = { name: 'Johnn Doe' }
+      delete member.image
 
       const response = await request(app)
         .post(URL)
@@ -91,8 +89,7 @@ describe('/members', () => {
     })
 
     it('should add a new member to the database', async () => {
-      const member = { name: 'Johnn Doe', image: 'some.image.url' }
-
+      /* Act */
       const response = await request(app)
         .post(URL)
         .set({ 'x-access-token': token })
@@ -109,14 +106,13 @@ describe('/members', () => {
     })
 
     it('should return the new member', async () => {
-      const member = { name: 'Johnn Doe', image: 'some.image.url' }
-
+      /* Act */
       const response = await request(app)
         .post(URL)
         .set({ 'x-access-token': token })
         .send(member)
-      const createdMember = response.body.body
 
+      const createdMember = response.body.body
       expect(response.status).to.be.equal(201)
       expect(createdMember).to.have.property('id').to.not.be.null
       expect(createdMember).to.have.property('name').equal('Johnn Doe')
@@ -131,8 +127,7 @@ describe('/members', () => {
     const updateURL = `${URL}/${memberId}`
 
     it('should reject the PUT request without token', async () => {
-      const member = { name: 'Johnn Doe', image: 'some.image.url' }
-
+      /* Act */
       const response = await request(app).put(updateURL).send(member)
 
       expect(response.status).to.be.equal(401)
@@ -142,7 +137,6 @@ describe('/members', () => {
     })
 
     it('should reject the PUT request from non admin user', async () => {
-      const member = { name: 'Johnn Doe', image: 'some.image.url' }
       token = generateToken({ name: 'test' })
 
       const response = await request(app)
@@ -158,7 +152,7 @@ describe('/members', () => {
     })
 
     it('should reject the PUT request with missing name property', async () => {
-      const member = { image: 'some.image.url' }
+      delete member.name
 
       const response = await request(app)
         .put(updateURL)
@@ -173,7 +167,7 @@ describe('/members', () => {
     })
 
     it('should reject the PUT request with missing image property', async () => {
-      const member = { name: 'Johnn Doe' }
+      delete member.image
 
       const response = await request(app)
         .put(updateURL)
@@ -188,7 +182,6 @@ describe('/members', () => {
     })
 
     it('should reject the PUT request with wrong ID', async () => {
-      const member = { name: 'Johnn Doe', image: 'some.image.url' }
       const wrongId = '999999999999999'
 
       const response = await request(app)
@@ -203,7 +196,6 @@ describe('/members', () => {
     })
 
     it('should update a member in the database', async () => {
-      const member = { name: 'Johnn Doe', image: 'some.image.url' }
       const newMember = await addMember(member)
 
       const response = await request(app)
@@ -219,7 +211,6 @@ describe('/members', () => {
     })
 
     it('should return the updated member', async () => {
-      const member = { name: 'Johnn Doe', image: 'some.image.url' }
       const newMember = await addMember(member)
 
       const response = await request(app)
@@ -290,55 +281,51 @@ describe('/members', () => {
         .match(/not found/i)
     })
 
-    ///////////////////// WIP
-    // it('should delete the member in the data base', async () => {
-    //   /* Act */
-    //   const response = await request(app)
-    //     .delete(`${URL}/${newMember.id}`)
-    //     .set({ 'x-access-token': token })
-    //     .send()
+    it('should delete the member in the data base', async () => {
+      /* Act */
+      const response = await request(app)
+        .delete(`${URL}/${newMember.id}`)
+        .set({ 'x-access-token': token })
+        .send()
 
-    //   const deletedMember = await getMemberById(newMember.id)
-    //   console.log('--DELETED:', deletedMember)
+      try {
+        await getMemberById(newMember.id)
+      } catch (error) {
+        expect(error)
+          .to.have.property('message')
+          .match(/not found/i)
+      }
+    })
 
-    //   expect(deletedMember).to.throw()
-    //   // .match(/not found/)
+    it('should return the deleted member', async () => {
+      /* Act */
+      const response = await request(app)
+        .delete(`${URL}/${newMember.id}`)
+        .set({ 'x-access-token': token })
+        .send()
+      const deletedMember = response.body.body
 
-    //   //clean up
-    //   await deleteMemberPermanently(newMember.id)
-    // })
-
-    // it('should return the deleted member', async () => {
-    //   const member = { name: 'Johnn Doe', image: 'some.image.url' }
-    //   const newMember = await addMember(member)
-
-    //   const response = await request(app)
-    //     .delete(`${URL}/${newMember.id}`)
-    //     .set({ 'x-access-token': token })
-    //     .send()
-    //   const deletedMember = response.body.body
-
-    //   expect(response.status).to.be.equal(200)
-    //   expect(deletedMember).to.have.property('name').equal('Johnn Doe')
-    //   expect(deletedMember).to.have.property('image').equal('some.image.url')
-    //   expect(deletedMember).to.have.property('deletedAt').to.not.be.null
-    // })
+      expect(response.status).to.be.equal(200)
+      expect(deletedMember).to.have.property('name').equal('Johnn Doe')
+      expect(deletedMember).to.have.property('image').equal('some.image.url')
+      expect(deletedMember).to.have.property('deletedAt').to.not.be.null
+    })
   })
 
-  // describe('GET /', () => {
-  //   it('should return all members array', async () => {
-  //     const res = await request(app).get(URL)
+  describe('GET /', () => {
+    it('should return all members array', async () => {
+      const res = await request(app).get(URL)
 
-  //     expect(res.status).to.be.equal(200)
-  //     expect(res.body.body).to.be.an('array')
-  //   })
+      expect(res.status).to.be.equal(200)
+      expect(res.body.body).to.be.an('array')
+    })
 
-  //   it('should return members objects with "name" and "image" properties', async () => {
-  //     const res = await request(app).get(URL)
+    it('should return members objects with "name" and "image" properties', async () => {
+      const res = await request(app).get(URL)
 
-  //     expect(res.body.body[0]).to.be.an('object')
-  //     expect(res.body.body[0]).to.have.property('name')
-  //     expect(res.body.body[0]).to.have.property('image')
-  //   })
-  // })
+      expect(res.body.body[0]).to.be.an('object')
+      expect(res.body.body[0]).to.have.property('name')
+      expect(res.body.body[0]).to.have.property('image')
+    })
+  })
 })
