6116ea1cee453890fd4394f7c592c7b0a024ad6a
Jonatan Garbuyo <58077441+JonatanGarbuyo@users.noreply.github.com>
2022-01-06 09:30:48 -0300

    fix seeds - new link with optimized images (#83)

--- seeders/20211123062651-create-activities.js
+++ seeders/20211123062651-create-activities.js
@@ -7,28 +7,28 @@ module.exports = {
         {
           name: 'Comedor Comunitario',
           content: 'Gracias al esfuerzo de todos los donantes, pudimos garantizar, hasta el momento, 2293 entregas de donaciones en 493 comedores y merenderos de todo el país. Desde entonces, llevamos distribuidos casi 710 toneladas de alimento y más de 153 mil artículos de higiene y limpieza.',
-          image: 'https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2016/07/02174140/Mes-de-la-Sopa-Solidaria-1920-3.jpg',
+          image: 'https://cohorte-octubre-e8bd4748.s3.amazonaws.com/media/c48adbad-e605-4703-8ef7-b6d76822d083.jpg',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Recorridas por el barrio',
           content: 'Esta nueva organización por zonas, nos permite incluir nuevos voluntarios de manera paulatina y en sus barrios de origen. ¿De qué manera? Sumándolos a los grupos armados que ya tienen experiencia y, además, conocen a las personas que vamos a visitar en cada recorrida por el barrio.',
-          image: 'https://www.unicef.org/argentina/sites/unicef.org.argentina/files/styles/hero_desktop/public/DSC_5372_0.JPG?itok=JKeN-a-h',
+          image: 'https://cohorte-octubre-e8bd4748.s3.amazonaws.com/media/0846df8e-4fc0-4987-b61c-0e16c4cb5975.jpg',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Apoyo escolar',
           content: 'Durante los últimos meses, decenas de voluntarios de todo el país participaron de las tareas de difusión y contacto con escuelas de zonas rurales o alejadas de los centros urbanos. Invitar a los jóvenes que estuvieran finalizando el secundario y que quisieran continuar estudiando',
-          image: 'https://www.unicef.org/argentina/sites/unicef.org.argentina/files/styles/hero_desktop/public/UNI116793.jpg?itok=uVx7FLxT',
+          image: 'https://cohorte-octubre-e8bd4748.s3.amazonaws.com/media/4c509c35-6f8a-48aa-ae0f-953de809cf5d.jpg',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Guiso Solidario',
           content: 'A través de esta iniciativa, estaremos colaborando con platos de comida para 23 comedores de todo el país durante todo un mes. De este modo, haremos posible que cientos de chicos y sus familias puedan disfrutar de un plato caliente en sus mesas. Gracias.',
-          image: 'https://www.diariosumario.com.ar/u/fotografias/fotosnoticias/2020/5/6/22591.jpg',
+          image: 'https://cohorte-octubre-e8bd4748.s3.amazonaws.com/media/6e7180ba-ae2b-4173-8838-d3557ce1d17f.jpg',
           createdAt: new Date(),
           updatedAt: new Date()
         }], {});
